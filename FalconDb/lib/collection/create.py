4816334712c10dd4326b9177e017ec1ba6c309d8
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-06 11:08:48 -0400

    added file existing catch

--- FalconDb/lib/collection/create.py
+++ FalconDb/lib/collection/create.py
@@ -1,6 +1,7 @@
 from genericpath import exists
 import platform
 import os
+from typing import Text
 OS = platform.system()
 
 Collection_Model = {
@@ -10,20 +11,23 @@ Collection_Model = {
 }
 class Collection:
     def CreateParentDir():
-        COL_DIR = Collection.FindEnvData()+"\\.FalconDB"
-        if not dir.exists(COL_DIR):
-            os.mkdir(COL_DIR)
+        COL_DIR = Collection.FindEnvData()+"\\FalconDB"
+        if not exists(COL_DIR):
+         os.mkdir(COL_DIR)
+         print ("Created Datafolder")
     def FindEnvData():
         if OS == "Windows":
             return os.getenv("APPDATA")
     def CreateCollection(collection_name):
+            nme = Text.lower(collection_name)
             ENV = Collection.FindEnvData()
             Collection.CreateParentDir()
-            collection_path=Collection.FindEnvData()+"\\.FalconDB\\"+collection_name
-            COLLECTION_FOLDER = os.mkdir(collection_path, "rw")
-            
+            collection_path=Collection.FindEnvData()+"\\FalconDB\\"+nme
+            print(collection_path)
+            if not exists(collection_path):
+                 COLLECTION_FOLDER = os.mkdir(collection_path)
 
             
             
-Collection.CreateCollection()
+Collection.CreateCollection("Turtles")
     
