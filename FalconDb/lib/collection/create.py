cf9b6fcb59babc1309bc26e5017abe7e32f33617
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-06 10:59:51 -0400

    create data folders globaly

--- FalconDb/lib/collection/create.py
+++ FalconDb/lib/collection/create.py
@@ -1,12 +1,29 @@
+from genericpath import exists
 import platform
 import os
 OS = platform.system()
 
+Collection_Model = {
+    "Name",
+    "Id",
+    "Timestamp"
+}
 class Collection:
+    def CreateParentDir():
+        COL_DIR = Collection.FindEnvData()+"\\.FalconDB"
+        if not dir.exists(COL_DIR):
+            os.mkdir(COL_DIR)
     def FindEnvData():
         if OS == "Windows":
             return os.getenv("APPDATA")
-        
-    def CreateCollection(args):
+    def CreateCollection(collection_name):
             ENV = Collection.FindEnvData()
-            collection_path=Collection.FindEnvData()
+            Collection.CreateParentDir()
+            collection_path=Collection.FindEnvData()+"\\.FalconDB\\"+collection_name
+            COLLECTION_FOLDER = os.mkdir(collection_path, "rw")
+            
+
+            
+            
+Collection.CreateCollection()
+    
